005cfd6e85a38bc4ad0124051021477e7d5c671e
iampeterjohn <peterjeche442@gmail.com>
2024-01-15 01:09:40 -1100

    junior school

--- src/views/Junior.vue
+++ src/views/Junior.vue
@@ -5,22 +5,7 @@
     import Blic from '@/components/Blic.vue'
     import 'animate.css'
     import { ref } from 'vue'
-    const nursSubj = ref([
-      'Phonics/Spelling',
-      'Moral Instruction',
-      'Knowledge of the world',
-      'Computer Appreciation',
-      'Literacy',
-      'Practical Life',
-      'Creativity',
-      'Music/rhymes',
-      'Handwriting',
-      'Numeracy',
-      'Social education',
-      'Story talk',
-      'Science talk',
-      'Presentation, social and health education'
-    ]);
+
     const priSubj = ref([
       'Physics',
       'Mathematics',
@@ -40,30 +25,6 @@
       'Geography',
       'Science'
     ]);
-    const secSubj = ref([
-      'Physics',
-      'Mathematics',
-      'English Language',
-      'Chemistry',
-      'Bioliogy',
-      'Social Studies',
-      'Handwritting',
-      'Library Studies',
-      'Civil education',
-      'Financial accounting',
-      'Arts And Crafts',
-      'Music',
-      'Yoruba',
-      'Further Mathematics',
-      'Literature in english',
-      'Ecnomics',
-      'Basic talk',
-      'Agricultural science',
-      'Basic technology',
-      'Commerce',
-      'Government',
-      'C.R.S'
-    ])
 </script>
 
 <template>
@@ -72,13 +33,14 @@
    <div class="container-fluid accreditation-and-affiliation-container-1">
         <div class="row">
             <div class="col col-sm col-lg ps-lg-5">
-                <h1 class="display-5 text-white text-capitalize px-4 py-2 animate__animated animate__slow animate__pulse animate__infinite">
-                   Our Curriculum
+                <h1 class="display-5 text-white text-capitalize px-4 py-2">
+                    Junior School
                 </h1>
             </div>
         </div>
    </div>
    <!-- closed -->
+    
    <!-- Executive-director-speech -->
     <div class="container-fluid-sm  bg-white accreditation-and-affiliation-container">
         <div class="container pt-5 px-3 px-lg-5 bg-light">
@@ -87,28 +49,42 @@
                      <p>
                        <span class="britis">British Lighthouse</span> school is committed to providing learning        opportunities that enrich and foster the academic development of our students. The school syllabus is a blend of British and Nigerian curricula. Thus, we encourage and guide students to give their best effort in pursuing excellence  within a caring, child-centered and supportive learning environment. We are  committed in seeing our students succeed and our aim is to produce a total child through a holistic programme that benchmarks best practices while being socially responsible.
                      </p>
-                     <h3>
-                        Learn more about our curriculum at every stage of your child’s academic career;
-                     </h3>
 
-                     <h6 class="textsecondary py-4">
-                        <span class="fw-bold fs-6">Pre-school</span> <span class="fw-bolder fs-5">:</span> 10 Months to 6 Years
-                     </h6>
-                     <p>
-                        British Lighthouse School journey begins with the pre-school program. We follow the Montessori scheme which establishes the basis for excellence in literacy, science, Sensorial, mathematics etc. This curriculum incucate love of learning, curiosity and rewarding critical thinking skills, also developing important social skills to students.In conclusion it provides a firm foundation on which to maximize physical, emotional, and intellectual development.
-                     </p>
-                     <div>
-                        <h6 class="textsecondary py-4">
-                           <span class="fw-bold fs-6">Subjects include</span> <span class="fw-bolder fs-5">:</span> 
-                        </h6>
-                     </div>
-                     <div>
-                       <ul>
-                          <li v-for="subj in nursSubj">
-                            {{ subj }}
-                          </li>
-                       </ul>
-                     </div>
+                        <div class="row row-gap-4 pt-4">
+                            <div class="col col-sm col-lg-12">
+                                <div>
+                                    Our aim is to make school enjoyable for our pupils; so we incorporate fun into learning and structure our learning environment to make learning easy for our pupils. Here are some more reasons why parents regard us one of the best junior schools in Lagos and entrust their children in our care:
+                                </div>
+
+                                <ul>
+                                    <li>
+                                        We engage our pupils in activities that enable them to develop self-confidence
+                                    </li>
+                                    <li>
+                                        We introduce our pupils to a foreign language, thereby offering them an opportunity to learn a unique form of communication
+                                    </li>
+                                    <li>
+                                        We guide and encourage positive behaviour among peers and promote collaboration through group activities.
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col col-sm col-lg-6">
+                                <img src="@/assets/img/britishlighthouse-2.jpg" class="img-fluid" alt="">
+                            </div>
+                            <div class="col col-sm col-lg-6">
+                                <img src="@/assets/img/Classroom_Students_ScienceLab_Indoor.webp" class="img-fluid" alt="">
+                            </div>
+                            <div class="col col-sm col-lg-6">
+                                <img src="@/assets/img/handicraft.jpg" class="img-fluid" alt="">
+                            </div>
+                            <div class="col col-sm col-lg-6">
+                                <img src="@/assets/img/musical.jpeg" class="img-fluid" alt="">
+                            </div>
+                        </div>
+
+                     <h3 class="mt-5">
+                        Learn more about our junior class curriculum 
+                     </h3>
 
                      <div>
                         <h6 class="textsecondary py-4">
@@ -133,28 +109,44 @@
                            </li>
                         </ul>
                      </div>
+                </div>
+            </div>
 
-                     <div>
-                        <h6 class="textsecondary py-4">
-                          <span class="fw-bold fs-6">Senior School</span> <span class="fw-bolder fs-5">:</span> 10 years to 16 years
-                        </h6>
-                     </div>
-                     <p>
-                        British Lighthouse School is located in a largely spaced expanse of Bekko Ransom Kutti Youth Centre Anthony, land with purpose built structures suitable for learning. The student overall development is paramount and this is represented in the core values of the school. To raise a total child, we place priority on emotional, physical, spiritual and psychological well-being of our students. The school runs a well-blended Curriculum (Nigeria and British Curricular)
-                     </p>
-                     <p>
-                         British Lighthouse School cares for children between the age of 12 years – 20 years.Start from year 7 (J.S.S.1), students are prepared to write the Checkpoint examination organized by Cambridge International Examination, Junior  School National Examination Council(NECO), Basic Education Certificate Examination( BECE). Also, Year 8 (J.S.S.2) students go on an exchange and field trip (FRENCH) while year 10 (S.S.S.1) students have their curriculum prepared towards writing the West African Examinations Council(WAEC), and National Examinations Council(NECO) examinations. Provisions are made for students who require Scholastic Aptitude Tes(SAT), Test of English as a Foreign Language (TOEFL) and Jiont Admission and Matriculation Board(JAMB).
-                     </p>
-                     <div>
-                        <ul>
-                           <li v-for="secondary in secSubj">
-                              {{ secondary }}
-                           </li>
-                        </ul>
-                     </div>
-                     <div class="down py-5">
-                        British Lighthouse School places high premium on extracurricular activities and participates in several competitions ranging from Sports, Literary, Debating, Musics, ICT, Spelling Bee and Quiz Competitions.
-                     </div>
+            <div class="row">
+                <div class="col col-sm col-lg">
+                    <h3 class=" text-center fw-bolder">
+                         What We Have & How We Do It 
+                    </h3>
+
+                    <ul>
+                        <li class="fw-bolder mt-3">
+                            Advisory & Guidance
+                        </li>
+                         <div>
+                            We offer the right challenges within a well-supervised and caring environment to enhance self-esteem and self-confidence.
+                         </div>
+
+                         <li class="fw-bolder mt-3">
+                            Specialist Tutors
+                        </li>
+                         <div>
+                            In situations where your child may experience difficulties in achieving satisfactory performance levels, our specialist tutors will provide relevant academic reinforcement.
+                         </div>
+
+                         <li class="fw-bolder mt-3">
+                            Stimulating Learning Environment
+                        </li>
+                         <div>
+                            We provide a stimulating and enjoyable learning environment to enable your child to develop his or her cognitive ability.
+                         </div>
+
+                         <li class="fw-bolder mt-3">
+                            Sporting facilities
+                        </li>
+                         <div>
+                            A list of our sports facilities includes 1 football pitches, 2 tennis courts, 2 basketball courts, 2 table-tennis tables, a badminton court, among others.
+                         </div>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -167,9 +159,9 @@
 
 <style scoped>
     .accreditation-and-affiliation-container-1{
-        background:linear-gradient(rgba(2, 2, 49, .4), rgba(2, 2, 49, .4)), url(../src/assets/img/school-foto-as-cover-foto.png);
+        background: linear-gradient(rgba(2, 2, 49, .4), rgba(2, 2, 49, .4)), url(../src/assets/img/britishlighthouse-2.jpg);
         width: 100%;
-        height: 400px;
+        height: 450px;
         display: flex;
         flex-direction: row;
         justify-content: center;
@@ -202,6 +194,14 @@
         text-align: start;
         padding: 10px 0;
     }  
+
+    img{
+        max-width: 100%;
+        width: 100%;
+        height: 270px;
+        object-fit: cover;
+        cursor: pointer;
+    }
 
     @media screen and (max-width: 992px) {
         accreditation-and-affiliation-container{
