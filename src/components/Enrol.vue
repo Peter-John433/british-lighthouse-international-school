acc63f3e3f700466874755fa2513b598f58aa959
iampeterjohn <peterjeche442@gmail.com>
2024-01-16 02:38:46 -1100

    enrol update

--- src/components/Enrol.vue
+++ src/components/Enrol.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 
 </script>
 
@@ -91,4 +91,214 @@
       color: #000;
       z-index: 10;
    }
+</style> -->
+<script setup>
+
+</script>
+
+<template>
+   <div>
+      <div class="container-fluid bg-white  pt-5 ">
+         <div class="container py-5">
+            <div class="row row-gap-4 column-gap mx-auto text-center">
+               <div class="col col-sm-6 col-lg-4 d-flex flex-column flex-lg-row justify-content-center align-items-center ">
+                 <router-link :to="{name: 'Pre'}">
+                     <div class="height-1">
+                        <img src="@/assets/img/Private-education.jpg" class="img-fluid" alt="">
+                        <div class="body-text-1 d-flex justify-content-center align-items-center ">
+                            <div class="first-body-text text-center">
+                               <h5 class="fw-bold">Pre School</h5>
+                            </div>
+                            <div class="arrow">
+                                <img src="@/assets/img/Chevron.png" alt="">
+                            </div>
+                        </div>
+                     </div>
+                 </router-link>
+               </div>
+
+               <div class="col col-sm-6 col-lg-4 d-flex flex-column flex-lg-row justify-content-center align-items-center">
+                 <router-link :to="{name: 'Junior'}">
+                     <div class="height-2">
+                        <img src="@/assets/img/Private-education.jpg" class="img-fluid" alt="">
+                        <div class="body-text-2 d-flex justify-content-center align-items-center ">
+            
+                           <div class="second-body-text">
+                                <h5 class="fw-bold text-center">Junior School</h5>
+                           </div>
+                           <div class="arrow">
+                                <img src="@/assets/img/Chevron.png" alt="">
+                            </div>
+                        </div>
+                     </div>
+                 </router-link>
+               </div> 
+
+               <div class="col col-sm col-lg-4 d-flex flex-column flex-lg-row justify-content-center align-items-center">
+                 <router-link :to="{name: 'Senior'}">
+                     <div class="height-3">
+                        <img src="@/assets/img/Private-education.jpg" class="img-fluid" alt="">
+                        <div class="body-text-3 d-flex justify-content-center align-items-center">
+                            
+                            <div class="third-body-text">
+                                <h5 class="fw-bold text-center">Senior School</h5>
+                            </div>
+                            <div class="arrow">
+                                <img src="@/assets/img/Chevron.png" alt="">
+                            </div>
+                        </div>
+                     </div>
+                 </router-link>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+
+   <router-view />
+</template>
+
+<style scoped>
+   .h2{
+      font-weight: 900;
+     color: rgb(1, 1, 31);
+   }
+
+   img{
+      width: 100%;
+      height: 250px;
+      object-fit: cover;
+   }
+   .height-1{
+      position: relative;
+      transition: all 200ms ease-in-out;
+      width: 380px;
+      height: 230px;
+   }
+   .height-1 img{
+      height: 230px;
+   }
+   
+   .body-text-1{
+      position: absolute;
+      bottom: 0%;
+      left: 0%;
+      right: 0%;
+      top: 0%;
+      font-size: 14px;
+      font-weight: bold;
+      color: #fff;
+      z-index: 99;
+      background-color: rgba(252, 0, 0, 0.6);
+   }
+
+   .body-text-1 img{
+      width: 100px;
+      height: 60px;
+      object-fit: contain;
+   }
+   .first-body-text{
+      width: 300px;
+      padding: 0;
+   }
+
+
+
+   .height-2{
+      position: relative;
+      transition: all 200ms ease-in-out;
+      width: 380px;
+      height: 230px;
+   }
+   .height-2 img{
+      height: 230px;
+   }
+
+   .body-text-2{
+      position: absolute;
+      bottom: 0%;
+      left: 0%;
+      right: 0%;
+      top: 0%;
+      font-size: 14px;
+      font-weight: bold;
+      color: #fff;
+      background-color: rgba(0, 0, 0, 0.6);
+   }
+    
+   .body-text-2 img{
+      width: 100px;
+      height: 60px;
+      object-fit: contain;
+   }
+   .second-body-text{
+      width: 300px;
+      padding: 0;
+   }
+
+
+
+   .height-3{
+      position: relative;
+      transition: all 200ms ease-in-out;
+      width: 380px;
+      height: 230px;
+   }
+   .height-3 img{
+      height: 230px;
+   }
+
+   .body-text-3{
+      position: absolute;
+      bottom: 0%;
+      left: 0%;
+      right: 0%;
+      top: 0%;
+      font-size: 14px;
+      font-weight: bold;
+      color: #fff;
+      background-color: rgba(252, 0, 0, 0.6);
+   }
+    
+   .body-text-3 img{
+      width: 100px;
+      height: 60px;
+      object-fit: contain;
+   }
+   .third-body-text{
+      width: 300px;
+      padding: 0;
+   }
+
+   .arrow{
+      position: absolute;
+      bottom: -7%;
+      z-index: 99;
+   }
+
+   .arrow img{
+      width: 30px;
+      height: 30px;
+      border: 1px solid black;
+      border-radius: 50%;
+      padding: 5px;
+      background-color: white;
+   }
+
+   @media screen and (max-width: 992px) {
+    .arrow{
+      position: absolute;
+      bottom: 10%;
+      
+     }
+        
+    }
+
+    @media screen and (max-width: 576px) {
+     .arrow{
+      position: absolute;
+      bottom: 10%;
+      
+     }
+    }
 </style>
